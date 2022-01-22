fa28ab91e0af1bd2f6291f32b9952e860803978b
Jonathan Sergio Rodriguez Alvarez <joni_la12_elnacio@hotmail.com>
2022-01-21 22:11:04 -0300

    Termine seccion de proyectos y la seccion de recumen, arregle algunos detalles.

--- index.js
+++ index.js
@@ -2,15 +2,43 @@ $(document).ready(function(){
     $(window).scroll(function(){
       var scroll = $(window).scrollTop();
       if (scroll > 30) {
-        $(".sticky").addClass("azul");
-        $(".sticky").removeClass("negro"); 
+        $(".sticky").addClass("efectoTransparente");
+        $(".sticky").removeClass("efectoNaranja");
+        $(".sticky").removeClass("efectoAzul");
+        $(".sticky").removeClass("efectoAzulFuerte");
       }
       else{
-        $(".sticky").removeClass("azul");  	
+        $(".sticky").removeClass("efectoTransparente");  	
       }
       if (scroll > 550) {
-        $(".sticky").addClass("negro");
-        $(".sticky").removeClass("azul"); 
+        $(".sticky").addClass("efectoNaranja");
+        $(".sticky").removeClass("efectoTransparente");
+        $(".sticky").removeClass("efectoAzul");
+        $(".sticky").removeClass("efectoAzulFuerte");
+      }
+      if (scroll > 1200) {
+        $(".sticky").addClass("efectoTransparente");
+        $(".sticky").removeClass("efectoNaranja");
+        $(".sticky").removeClass("efectoAzul");
+        $(".sticky").removeClass("efectoAzulFuerte");
+      }
+      if (scroll > 2700) {
+        $(".sticky").addClass("efectoAzul");
+        $(".sticky").removeClass("efectoNaranja");
+        $(".sticky").removeClass("efectoTransparente");
+        $(".sticky").removeClass("efectoAzulFuerte");
+      }
+      if (scroll > 3800) {
+        $(".sticky").addClass("efectoTransparente");
+        $(".sticky").removeClass("efectoNaranja");
+        $(".sticky").removeClass("efectoAzul");
+        $(".sticky").removeClass("efectoAzulFuerte");
+      }
+      if (scroll > 4500) {
+        $(".sticky").addClass("efectoAzulFuerte");
+        $(".sticky").removeClass("efectoNaranja");
+        $(".sticky").removeClass("efectoAzul");
+        $(".sticky").removeClass("efectoTransparente"); 
       }
     })
   })
