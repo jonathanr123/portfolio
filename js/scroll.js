31d57b0085266fd14d88d36d54e0d8f73bdf029d
Jonathan Sergio Rodriguez Alvarez <joni_la12_elnacio@hotmail.com>
2022-01-23 05:19:43 -0300

    Termine footer y agregue loader

---
+++ js/scroll.js
@@ -0,0 +1,40 @@
+window.addEventListener('scroll', function()  {
+    let elements = document.getElementsByClassName('scroll-content');
+    let screenSize = window.innerHeight;
+    
+          for(var i = 0; i < elements.length; i++) {
+          var element = elements[i];
+  
+          if(element.getBoundingClientRect().top < screenSize) {
+            element.classList.add('visible');
+          } else {
+            element.classList.remove('visible');
+          }
+  
+        }
+  });
+
+window.addEventListener('scroll', function()  {
+    let elements = document.getElementsByClassName('scroll-content-bottom');
+    let screenSize = window.innerHeight;
+    
+          for(var i = 0; i < elements.length; i++) {
+          var element = elements[i];
+  
+          if(element.getBoundingClientRect().bottom < screenSize) {
+            element.classList.add('visible');
+          } else {
+            element.classList.remove('visible');
+          }
+  
+        }
+  });
+
+
+  $(window).on('load', function () {
+    setTimeout(function () {
+  $(".loader").css({visibility:"hidden",opacity:"0",height:"0",transition: "all 1s"})
+}, 2000);
+   
+});
+
